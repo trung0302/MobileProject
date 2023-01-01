75f236acf6d4a05da012825c94aa4c9d5b769455
Trung <thanhtrungvnger@gmail.com>
2023-01-01 21:26:44 +0700

    aa

---
+++ Project/Project/Response/UserResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Response
+{
+    public class UserResponse
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string role { get; set; }
+        public string token { get; set; }
+    }
+}
