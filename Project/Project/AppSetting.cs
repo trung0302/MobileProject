ca27978c35606c330b520475ed0c42ec9bcff168
QuocBao1097 <20521097@gm.uit.edu.vn>
2023-01-13 15:09:21 +0700

    done

--- Project/Project/AppSetting.cs
+++ Project/Project/AppSetting.cs
@@ -7,7 +7,7 @@ namespace Project
 {
     public static class AppSettings
     {
-        public static string ApiUrl = DeviceInfo.Platform == DevicePlatform.Android ? "http://192.168.1.6:9372/" : "https://localhost:44312/";
-        //public static string ApiUrl = "http://192.168.1.6:9372/";
+        public static string ApiUrl = DeviceInfo.Platform == DevicePlatform.Android ? "http://192.168.1.5:9372/" : "https://localhost:44312/";
+        //public static string ApiUrl = "http://192.168.1.5:9372/";
     }
 }
